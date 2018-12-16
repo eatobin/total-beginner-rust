4d5de5e33d297f4e7330e4e8f9fa00b95ccd424c
Eric Tobin <eatobin@gmail.com>
2018-12-15 18:49:38 -0600

    works!

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,45 @@
-struct Borrower<'a> {
-    name: &'a str,
-    max_books: u8,
-}
+mod borrower {
+    #[derive(Debug)]
+    pub struct Borrower<'a> {
+        name: &'a str,
+        max_books: u8,
+    }
 
-impl<'a> Borrower<'a> {
-    fn build_borrower(name: &str, max_books: u8) -> Borrower {
-        Borrower {
-            name,
-            max_books,
+    pub fn build_borrower(name: &str, max_books: u8) -> Borrower {
+        Borrower { name, max_books }
+    }
+
+    #[allow(dead_code)]
+    #[allow(unused_variables)]
+    impl<'a> Borrower<'a> {
+        pub fn set_name(&mut self, n: &'a str) {
+            self.name = n;
         }
-    }
 
-    fn set_name(&mut self, n: &'a str) -> () {
-        self.name;
-    }
+        pub fn set_max_books(&mut self, mb: u8) {
+            self.max_books = mb;
+        }
 
-    fn set_max_books(&mut self, mb: u8) -> () {
-        self.max_books;
-    }
-
-    fn borrower_to_string(&self) -> &str {
-        &(self.name.to_owned() + " (" + self.max_books.to_string() + " books)")
+        // fn borrower_to_string(&self) -> &str {
+        //     &(self.name.to_owned() + " (" + self.max_books.to_string() + " books)")
+        // }
     }
 }
-
 fn main() {
-    let b1 = Borrower::build_borrower("me", 66);
-    println!("{:?}", b1.name);
+    let mut b1 = borrower::build_borrower("me", 66);
+    println!("{:?}", b1);
+    b1.set_name("newish");
+    println!("{:?}", b1);
+    b1.set_max_books(88);
+    println!("{:?}", b1);
 }
+
+// #[cfg(test)] // only compiles when running tests
+// use super::Borrower::build_borrower; // import root greet function
+// mod tests {
+//     #[test]
+//     fn test_greet() {
+
+//         assert_eq!("Hello, world!", Borrower::build_borrower("me", 66));
+//     }
+// }
