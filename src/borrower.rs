cd4f4c061f7c310da37ca97a10103780f3021996
Eric Tobin <eatobin@gmail.com>
2018-12-19 16:38:56 -0600

    dev branch

--- src/borrower.rs
+++ src/borrower.rs
@@ -1,16 +1,19 @@
 #[derive(Debug, PartialEq)]
-pub struct Borrower<'a> {
-    name: &'a str,
+pub struct Borrower {
+    name: String,
     max_books: u8,
 }
 
-pub fn build_borrower(name: &str, max_books: u8) -> Borrower {
-    Borrower { name, max_books }
-}
+//pub fn build_borrower(name: &str, max_books: u8) -> Borrower {
+//    Borrower { name, max_books }
+//}
 
-impl<'a> Borrower<'a> {
-    pub fn set_name(&mut self, n: &'a str) {
-        self.name = n;
+impl Borrower {
+    pub fn new<S: Into<String>>(name: S, max_books: u8) -> Borrower {
+        Borrower { name: name.into(), max_books }
+    }
+    pub fn set_name<S: Into<String>>(&mut self, name: S) {
+        self.name = name.into();
     }
 
     pub fn set_max_books(&mut self, mb: u8) {
@@ -27,23 +30,23 @@ impl<'a> Borrower<'a> {
 }
 
 #[test]
-fn test_build_borrower() {
+fn test_new() {
     let br = Borrower {
-        name: "borrower1",
+        name: "borrower1".to_string(),
         max_books: 1,
     };
-    assert_eq!(br, build_borrower("borrower1", 1));
+    assert_eq!(br, Borrower::new("borrower1", 1));
 }
 
 #[test]
 fn test_set_name() {
     let mut br1 = Borrower {
-        name: "borrower1",
+        name: "borrower1".to_string(),
         max_books: 1,
     };
-    br1.set_name("borrower2");
+    br1.set_name("borrower2".to_string());
     let br2 = Borrower {
-        name: "borrower2",
+        name: "borrower2".to_string(),
         max_books: 1,
     };
     assert_eq!(br2, br1);
@@ -52,12 +55,12 @@ fn test_set_name() {
 #[test]
 fn test_set_max_books() {
     let mut br1 = Borrower {
-        name: "borrower1",
+        name: "borrower1".to_string(),
         max_books: 1,
     };
     br1.set_max_books(2);
     let br2 = Borrower {
-        name: "borrower1",
+        name: "borrower1".to_string(),
         max_books: 2,
     };
     assert_eq!(br2, br1);
@@ -66,7 +69,7 @@ fn test_set_max_books() {
 #[test]
 fn test_borrower_to_string() {
     let br1 = Borrower {
-        name: "borrower1",
+        name: "borrower1".to_string(),
         max_books: 1,
     };
     assert_eq!("borrower1 (1 books)", br1.borrower_to_string());
