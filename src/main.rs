63d58baff638a4fdff07c5130afd2c0dcad20bb1
Eric Tobin <eatobin@gmail.com>
2018-12-21 14:17:44 -0600

    noodles

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@
 mod borrower;
 
 fn main() {
-    println!("{}", "nothing here!")
+    let mut noodles: String = "noodles".to_string();
+    let oodles: &mut str = &mut noodles[1..];
+    oodles.make_ascii_uppercase();
+    println!("{}", oodles);
 }
