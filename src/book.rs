4b11035786fe55cbc04ac5e57d0f27e468ad06a4
Eric Tobin <eatobin@gmail.com>
2018-12-23 19:34:37 -0600

    to_owned()

--- src/book.rs
+++ src/book.rs
@@ -9,22 +9,22 @@ pub struct Book {
 
 impl Book {
     pub fn new(title: &str, author: &str) -> Book {
-        Book { title: title.to_string(), author: author.to_string(), borrower: None }
+        Book { title: title.to_owned(), author: author.to_owned(), borrower: None }
     }
-    pub fn set_title(&mut self, title: &str) { self.title = title.to_string() }
-    pub fn set_author(&mut self, author: &str) { self.author = author.to_string() }
+    pub fn set_title(&mut self, title: &str) { self.title = title.to_owned() }
+    pub fn set_author(&mut self, author: &str) { self.author = author.to_owned() }
     pub fn set_borrower(&mut self, borrower: Option<Borrower>) { self.borrower = borrower }
 
     pub fn available_string(&self) -> String {
         match &self.borrower {
             Some(br) => format!("{} {}", "Checked out to", &(br.name)),
-            None => "Available".to_string()
+            None => "Available".to_owned()
         }
     }
 
 //    pub fn borrower_to_string(&self) -> String {
-//        let mut bs: String = self.name.to_string() + " (";
-//        let mb: String = self.max_books.to_string();
+//        let mut bs: String = self.name.to_owned() + " (";
+//        let mb: String = self.max_books.to_owned();
 //        bs.push_str(&mb);
 //        bs += " books)"; // an alternate way to concatenate
 //        bs
@@ -34,8 +34,8 @@ impl Book {
 #[test]
 fn test_new() {
     let bk = Book {
-        title: "title1".to_string(),
-        author: "author1".to_string(),
+        title: "title1".to_owned(),
+        author: "author1".to_owned(),
         borrower: None,
     };
     assert_eq!(bk, Book::new("title1", "author1"));
@@ -44,14 +44,14 @@ fn test_new() {
 #[test]
 fn test_set_title() {
     let mut bk1 = Book {
-        title: "title1".to_string(),
-        author: "author1".to_string(),
+        title: "title1".to_owned(),
+        author: "author1".to_owned(),
         borrower: None,
     };
     bk1.set_title("title2");
     let bk2 = Book {
-        title: "title2".to_string(),
-        author: "author1".to_string(),
+        title: "title2".to_owned(),
+        author: "author1".to_owned(),
         borrower: None,
     };
     assert_eq!(bk2, bk1);
@@ -60,14 +60,14 @@ fn test_set_title() {
 #[test]
 fn test_set_author() {
     let mut bk1 = Book {
-        title: "title1".to_string(),
-        author: "author1".to_string(),
+        title: "title1".to_owned(),
+        author: "author1".to_owned(),
         borrower: None,
     };
     bk1.set_author("author2");
     let bk2 = Book {
-        title: "title1".to_string(),
-        author: "author2".to_string(),
+        title: "title1".to_owned(),
+        author: "author2".to_owned(),
         borrower: None,
     };
     assert_eq!(bk2, bk1);
@@ -76,21 +76,21 @@ fn test_set_author() {
 #[test]
 fn test_set_borrower() {
     let mut bk1 = Book {
-        title: "title1".to_string(),
-        author: "author1".to_string(),
+        title: "title1".to_owned(),
+        author: "author1".to_owned(),
         borrower: None,
     };
     bk1.set_borrower(Some(Borrower
         {
-            name: "borrower1".to_string(),
+            name: "borrower1".to_owned(),
             max_books: 1,
         }));
     let bk2 = Book {
-        title: "title1".to_string(),
-        author: "author1".to_string(),
+        title: "title1".to_owned(),
+        author: "author1".to_owned(),
         borrower: Some(Borrower
             {
-                name: "borrower1".to_string(),
+                name: "borrower1".to_owned(),
                 max_books: 1,
             }),
     };
@@ -100,18 +100,18 @@ fn test_set_borrower() {
 #[test]
 fn test_book_to_string() {
     let bk1 = Book {
-        title: "title1".to_string(),
-        author: "author1".to_string(),
+        title: "title1".to_owned(),
+        author: "author1".to_owned(),
         borrower: None,
     };
     assert_eq!(bk1.available_string(), "Available");
 
     let bk2 = Book {
-        title: "title1".to_string(),
-        author: "author1".to_string(),
+        title: "title1".to_owned(),
+        author: "author1".to_owned(),
         borrower: Some(Borrower
             {
-                name: "borrower1".to_string(),
+                name: "borrower1".to_owned(),
                 max_books: 1,
             }),
     };
--- src/borrower.rs
+++ src/borrower.rs
@@ -6,15 +6,15 @@ pub struct Borrower {
 
 impl Borrower {
     pub fn new(name: &str, max_books: u8) -> Borrower {
-        Borrower { name: name.to_string(), max_books }
+        Borrower { name: name.to_owned(), max_books }
     }
-    pub fn set_name(&mut self, name: &str) { self.name = name.to_string() }
+    pub fn set_name(&mut self, name: &str) { self.name = name.to_owned() }
     pub fn set_max_books(&mut self, mb: u8) {
         self.max_books = mb
     }
 
     pub fn borrower_to_string(&self) -> String {
-        let mut bs: String = self.name.to_string() + " (";
+        let mut bs: String = self.name.to_owned() + " (";
         let mb: String = self.max_books.to_string();
         bs.push_str(&mb);
         bs += " books)"; // an alternate way to concatenate
@@ -25,7 +25,7 @@ impl Borrower {
 #[test]
 fn test_new() {
     let br = Borrower {
-        name: "borrower1".to_string(),
+        name: "borrower1".to_owned(),
         max_books: 1,
     };
     assert_eq!(br, Borrower::new("borrower1", 1));
@@ -34,12 +34,12 @@ fn test_new() {
 #[test]
 fn test_set_name() {
     let mut br1 = Borrower {
-        name: "borrower1".to_string(),
+        name: "borrower1".to_owned(),
         max_books: 1,
     };
     br1.set_name("borrower2");
     let br2 = Borrower {
-        name: "borrower2".to_string(),
+        name: "borrower2".to_owned(),
         max_books: 1,
     };
     assert_eq!(br2, br1);
@@ -48,12 +48,12 @@ fn test_set_name() {
 #[test]
 fn test_set_max_books() {
     let mut br1 = Borrower {
-        name: "borrower1".to_string(),
+        name: "borrower1".to_owned(),
         max_books: 1,
     };
     br1.set_max_books(2);
     let br2 = Borrower {
-        name: "borrower1".to_string(),
+        name: "borrower1".to_owned(),
         max_books: 2,
     };
     assert_eq!(br2, br1);
@@ -62,7 +62,7 @@ fn test_set_max_books() {
 #[test]
 fn test_borrower_to_string() {
     let br1 = Borrower {
-        name: "borrower1".to_string(),
+        name: "borrower1".to_owned(),
         max_books: 1,
     };
     assert_eq!("borrower1 (1 books)", br1.borrower_to_string());
