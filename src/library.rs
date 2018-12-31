f5274506fced76fca01f913aef25fabc6e12bbfb
Eric Tobin <eatobin@gmail.com>
2018-12-31 11:51:40 -0600

    test module

--- src/library.rs
+++ src/library.rs
@@ -10,101 +10,107 @@ pub fn add_item<T: PartialEq>(x: T, mut xs: Vec<T>) -> Vec<T> {
     }
 }
 
-#[test]
-fn test_add_item() {
-    let br1 = Borrower {
-        name: String::from("Borrower1"),
-        max_books: 1,
-    };
-    let mut brs: Vec<Borrower> = Vec::new();
-
-    brs = add_item(br1, brs);
-    assert_eq!(
-        vec![Borrower {
-            name: String::from("Borrower1"),
-            max_books: 1,
-        },],
-        brs
-    );
-
-    brs = add_item(Borrower::new("Borrower1", 1), brs);
-    assert_eq!(
-        vec![Borrower {
-            name: String::from("Borrower1"),
-            max_books: 1,
-        },],
-        brs
-    );
-
-    let bk1 = Book {
-        title: "Title1".to_owned(),
-        author: String::from("Author1"),
-        borrower: None,
-    };
-    let mut bks: Vec<Book> = Vec::new();
-
-    bks = add_item(bk1, bks);
-    assert_eq!(
-        vec![Book {
-            title: "Title1".to_owned(),
-            author: "Author1".to_owned(),
-            borrower: None,
-        },],
-        bks
-    );
-
-    bks = add_item(Book::new("Title1", "Author1"), bks);
-    assert_eq!(
-        vec![Book {
-            title: "Title1".to_owned(),
-            author: "Author1".to_owned(),
-            borrower: None,
-        },],
-        bks
-    );
-}
-
 pub fn remove_book(bk: Book, mut bks: Vec<Book>) -> Vec<Book> {
     bks.retain(|i| i != &bk);
     bks
 }
 
-#[test]
-fn test_remove_book() {
-    let mut bks1 = vec![
-        Book {
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add_item() {
+        let br1 = Borrower {
+            name: String::from("Borrower1"),
+            max_books: 1,
+        };
+        let mut brs: Vec<Borrower> = Vec::new();
+
+        brs = add_item(br1, brs);
+        assert_eq!(
+            vec![Borrower {
+                name: String::from("Borrower1"),
+                max_books: 1,
+            }, ],
+            brs
+        );
+
+        brs = add_item(Borrower::new("Borrower1", 1), brs);
+        assert_eq!(
+            vec![Borrower {
+                name: String::from("Borrower1"),
+                max_books: 1,
+            }, ],
+            brs
+        );
+
+        let bk1 = Book {
             title: "Title1".to_owned(),
-            author: "Author1".to_owned(),
+            author: String::from("Author1"),
             borrower: None,
-        },
-        Book {
+        };
+        let mut bks: Vec<Book> = Vec::new();
+
+        bks = add_item(bk1, bks);
+        assert_eq!(
+            vec![Book {
+                title: "Title1".to_owned(),
+                author: "Author1".to_owned(),
+                borrower: None,
+            }, ],
+            bks
+        );
+
+        bks = add_item(Book::new("Title1", "Author1"), bks);
+        assert_eq!(
+            vec![Book {
+                title: "Title1".to_owned(),
+                author: "Author1".to_owned(),
+                borrower: None,
+            }, ],
+            bks
+        );
+    }
+
+    #[test]
+    fn test_remove_book() {
+        let mut bks1 = vec![
+            Book {
+                title: "Title1".to_owned(),
+                author: "Author1".to_owned(),
+                borrower: None,
+            },
+            Book {
+                title: "Title2".to_owned(),
+                author: "Author2".to_owned(),
+                borrower: None,
+            },
+        ];
+        bks1 = remove_book(
+            Book {
+                title: "Title1".to_owned(),
+                author: "Author1".to_owned(),
+                borrower: None,
+            },
+            bks1,
+        );
+        let bks2 = vec![Book {
             title: "Title2".to_owned(),
             author: "Author2".to_owned(),
             borrower: None,
-        },
-    ];
-    bks1 = remove_book(
-        Book {
-            title: "Title1".to_owned(),
-            author: "Author1".to_owned(),
-            borrower: None,
-        },
-        bks1,
-    );
-    let bks2 = vec![Book {
-        title: "Title2".to_owned(),
-        author: "Author2".to_owned(),
-        borrower: None,
-    }];
-    assert_eq!(bks1, bks2);
+        }];
+        assert_eq!(bks1, bks2);
 
-    bks1 = remove_book(
-        Book {
-            title: "Title1".to_owned(),
-            author: "Author1".to_owned(),
-            borrower: None,
-        },
-        bks1,
-    );
-    assert_eq!(bks1, bks2);
+        bks1 = remove_book(
+            Book {
+                title: "Title1".to_owned(),
+                author: "Author1".to_owned(),
+                borrower: None,
+            },
+            bks1,
+        );
+        assert_eq!(bks1, bks2);
+    }
 }
