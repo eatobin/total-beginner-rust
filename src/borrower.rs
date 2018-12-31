37bdf4a3217565c68bbb9f8dd9dd05e7f1fa34ab
Eric Tobin <eatobin@gmail.com>
2018-12-31 11:45:25 -0600

    test module

--- src/borrower.rs
+++ src/borrower.rs
@@ -29,57 +29,63 @@ impl Borrower {
     }
 }
 
-#[test]
-fn test_new_borrower() {
-    let br = Borrower {
-        name: String::from("Borrower1"),
-        max_books: 1,
-    };
-    assert_eq!(br, Borrower::new("Borrower1", 1));
-}
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
 
-#[test]
-fn test_get_name() {
-    let br = Borrower {
-        name: "Borrower1".to_owned(),
-        max_books: 1,
-    };
-    assert_eq!(Borrower::get_name(&br), "Borrower1");
-}
+    #[test]
+    fn test_new_borrower() {
+        let br = Borrower {
+            name: String::from("Borrower1"),
+            max_books: 1,
+        };
+        assert_eq!(br, Borrower::new("Borrower1", 1));
+    }
 
-#[test]
-fn test_set_name() {
-    let mut br1 = Borrower {
-        name: "Borrower1".to_owned(),
-        max_books: 1,
-    };
-    br1.set_name("borrower2");
-    let br2 = Borrower {
-        name: "borrower2".to_owned(),
-        max_books: 1,
-    };
-    assert_eq!(br2, br1);
-}
+    #[test]
+    fn test_get_name() {
+        let br = Borrower {
+            name: "Borrower1".to_owned(),
+            max_books: 1,
+        };
+        assert_eq!(Borrower::get_name(&br), "Borrower1");
+    }
 
-#[test]
-fn test_set_max_books() {
-    let mut br1 = Borrower {
-        name: "Borrower1".to_owned(),
-        max_books: 1,
-    };
-    br1.set_max_books(2);
-    let br2 = Borrower {
-        name: "Borrower1".to_owned(),
-        max_books: 2,
-    };
-    assert_eq!(br2, br1);
-}
+    #[test]
+    fn test_set_name() {
+        let mut br1 = Borrower {
+            name: "Borrower1".to_owned(),
+            max_books: 1,
+        };
+        br1.set_name("borrower2");
+        let br2 = Borrower {
+            name: "borrower2".to_owned(),
+            max_books: 1,
+        };
+        assert_eq!(br2, br1);
+    }
 
-#[test]
-fn test_borrower_to_string() {
-    let br1 = Borrower {
-        name: "Borrower1".to_owned(),
-        max_books: 1,
-    };
-    assert_eq!("Borrower1 (1 books)", br1.borrower_to_string());
+    #[test]
+    fn test_set_max_books() {
+        let mut br1 = Borrower {
+            name: "Borrower1".to_owned(),
+            max_books: 1,
+        };
+        br1.set_max_books(2);
+        let br2 = Borrower {
+            name: "Borrower1".to_owned(),
+            max_books: 2,
+        };
+        assert_eq!(br2, br1);
+    }
+
+    #[test]
+    fn test_borrower_to_string() {
+        let br1 = Borrower {
+            name: "Borrower1".to_owned(),
+            max_books: 1,
+        };
+        assert_eq!("Borrower1 (1 books)", br1.borrower_to_string());
+    }
 }
