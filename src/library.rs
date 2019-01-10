c0879564cb0d3f37b71981013dec3f02076272cc
Eric Tobin <eatobin@gmail.com>
2019-01-10 16:14:29 -0600

    library progress!!

--- src/library.rs
+++ src/library.rs
@@ -37,21 +37,21 @@ impl Library {
 
     // TODO add_item?
 
-    //    pub fn find_borrower(&self, name: &str) -> Option<&mut Borrower> {
-    //        self.borrowers
-    //            .iter_mut()
-    //            .find(|&br| Borrower::get_name(br) == name)
-    //    }
+    pub fn find_borrower(&mut self, name: &str) -> Option<&mut Borrower> {
+        self.borrowers
+            .iter_mut()
+            .find(|br| Borrower::get_name(br) == name)
+    }
+
+    pub fn find_book(&mut self, title: &str) -> Option<&mut Book> {
+        self.books
+            .iter_mut()
+            .find(|bk| Book::get_title(bk) == title)
+    }
+
+    // TODO find_item?
 }
 
-////pub fn find_borrower<'a>(
-////    name: &str,
-////    brs: &'a HashSet<Borrower>,
-////    f: &Fn(&'a Borrower) -> &str,
-////) -> Option<&'a Borrower> {
-////    brs.iter().find(|&i| f(i) == name)
-////}
-//
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -80,46 +80,36 @@ mod tests {
         assert_eq!(lib.get_books().len(), 1);
     }
 
-    //    //    #[test]
-    //    //    fn test_remove_book() {
-    //    //        let bk1 = Book::new("Title1", "Author1", Some(Borrower::new("Borrower1", 1)));
-    //    //        let bk2 = Book::new("Title2", "Author2", None);
-    //    //        let mut bks1: Vec<&Book> = vec![&bk1, &bk2];
-    //    //        let bks2: Vec<&Book> = vec![&bk1, &bk2];
-    //    //
-    //    //        bks1 = remove_book(&Book::new("Title22", "Author2", None), bks1);
-    //    //        assert_eq!(bks1, bks2);
-    //    //
-    //    //        let bks2: Vec<&Book> = vec![&bk1];
-    //    //
-    //    //        bks1 = remove_book(&Book::new("Title2", "Author2", None), bks1);
-    //    //        assert_eq!(bks1, bks2);
-    //    //    }
-    //    //
-    //    //    #[test]
-    //    //    fn test_find_item() {
-    //    //        let br1 = Borrower::new("Borrower1", 1);
-    //    //        let br2 = Borrower::new("Borrower2", 2);
-    //    //        let mut brs: Vec<&Borrower> = Vec::new();
-    //    //        brs = add_item(&br1, brs);
-    //    //        brs.push(&br2);
-    //    //
-    //    //        let fnd_br = find_item("Borrower1", &brs, &Borrower::get_name);
-    //    //        assert_eq!(fnd_br, Some(&&br1));
-    //    //
-    //    //        let fnd_br: Option<&&Borrower> = find_item("Borrower11", &brs, &Borrower::get_name);
-    //    //        assert_eq!(fnd_br, None);
-    //    //
-    //    //        let bk1 = Book::new("Title1", "Author1", None);
-    //    //        let bk2 = Book::new("Title2", "Author2", None);
-    //    //        let mut bks: Vec<&Book> = Vec::new();
-    //    //        bks.push(&bk1);
-    //    //        bks.push(&bk2);
-    //    //
-    //    //        let fnd_bk = find_item("Title1", &bks, &Book::get_title);
-    //    //        assert_eq!(fnd_bk, Some(&&bk1));
-    //    //
-    //    //        let fnd_bk = find_item("Title11", &bks, &Book::get_title);
-    //    //        assert_eq!(fnd_bk, None);
-    //    //    }
+    #[test]
+    fn test_find_borrower_or_book() {
+        let mut lib = Library::new();
+        let mut br1 = Borrower::new("Borrower1", 1);
+        let mut br2 = Borrower::new("Borrower1", 1);
+        let mut bk1 = Book::new("Title1", "Author1", Some(Borrower::new("Borrower1", 1)));
+        let mut bk2 = Book::new("Title1", "Author1", Some(Borrower::new("Borrower1", 1)));
+        lib.add_borrower(br1);
+        lib.add_borrower(br2);
+        lib.add_book(bk1);
+        lib.add_book(bk2);
+
+        let fnd_br = lib.find_borrower("Borrower1");
+
+        assert_eq!(fnd_br, Some(&mut Borrower::new("Borrower1", 1)));
+
+        let fnd_br = lib.find_borrower("Borrower11");
+        assert_eq!(fnd_br, None);
+
+        let fnd_bk = lib.find_book("Title1");
+        assert_eq!(
+            fnd_bk,
+            Some(&mut Book::new(
+                "Title1",
+                "Author1",
+                Some(Borrower::new("Borrower1", 1))
+            ))
+        );
+
+        let fnd_bk = lib.find_book("Title11");
+        assert_eq!(fnd_bk, None);
+    }
 }
