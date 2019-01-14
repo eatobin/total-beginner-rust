47ce55b5828a89563c19278c26dc830e1d5a79d3
Eric Tobin <eatobin@gmail.com>
2019-01-14 17:07:46 -0600

    in process

--- src/library.rs
+++ src/library.rs
@@ -126,39 +126,67 @@ mod tests {
         let bks = add_item(bks, bk2);
         assert_eq!(bks.len(), 1);
     }
-    //
-    //    #[test]
-    //    fn test_find_borrower_or_book() {
-    //        let mut lib = Library::new();
-    //        let mut br1 = Borrower::new("Borrower1", 1);
-    //        let mut br2 = Borrower::new("Borrower1", 1);
-    //        let mut bk1 = Book::new("Title1", "Author1", Some(Borrower::new("Borrower1", 1)));
-    //        let mut bk2 = Book::new("Title1", "Author1", Some(Borrower::new("Borrower1", 1)));
-    //        lib.add_borrower(br1);
-    //        lib.add_borrower(br2);
-    //        lib.add_book(bk1);
-    //        lib.add_book(bk2);
-    //
-    //        let fnd_br = lib.find_borrower("Borrower1");
-    //
-    //        assert_eq!(fnd_br, Some(&Borrower::new("Borrower1", 1)));
-    //
-    //        let fnd_br = lib.find_borrower("Borrower11");
-    //        assert_eq!(fnd_br, None);
-    //
-    //        let fnd_bk = lib.find_book("Title1");
-    //        assert_eq!(
-    //            fnd_bk,
-    //            Some(&mut Book::new(
-    //                "Title1",
-    //                "Author1",
-    //                Some(Borrower::new("Borrower1", 1))
-    //            ))
-    //        );
-    //
-    //        let fnd_bk = lib.find_book("Title11");
-    //        assert_eq!(fnd_bk, None);
-    //    }
+
+    #[test]
+    fn test_remove_book() {
+        let bk1 = Book::new("Title1", "Author1", Some(Borrower::new("Borrower1", 1)));
+        let bk2 = Book::new("Title1", "Author1", Some(Borrower::new("Borrower1", 1)));
+        let bk3 = Book::new("Title3", "Author3", Some(Borrower::new("Borrower3", 3)));
+        let bk4 = Book::new("Title1", "Author1", Some(Borrower::new("Borrower1", 1)));
+        let mut bks: Vec<Book> = Vec::new();
+
+        assert_eq!(bks.len(), 0);
+        let bks = add_item(bks, bk1);
+        assert_eq!(bks.len(), 1);
+
+        let bks = remove_book(bks, bk2);
+        assert_eq!(bks.len(), 0);
+
+        assert_eq!(bks.len(), 0);
+        let bks = add_item(bks, bk3);
+        assert_eq!(bks.len(), 1);
+
+        let bks = remove_book(bks, bk4);
+        assert_eq!(bks.len(), 1);
+    }
+
+        #[test]
+        fn test_find_borrower_or_book() {
+            let br1 = Borrower::new("Borrower1", 1);
+            let br2 = Borrower::new("Borrower1", 1);
+            let bk1 = Book::new("Title1", "Author1", Some(Borrower::new("Borrower1", 1)));
+            let bk2 = Book::new("Title1", "Author1", Some(Borrower::new("Borrower1", 1)));
+            let brs1: Vec<Borrower> = Vec::new();
+            let bks1: Vec<Book> = Vec::new();
+            let brs2: Vec<Borrower> = Vec::new();
+            let bks2: Vec<Book> = Vec::new();
+
+            assert_eq!(brs1.len(), 0);
+            let brs1 = add_item(brs1, br1);
+            assert_eq!(brs1.len(), 1);
+
+            let fnd_br = find_borrower(brs1, "Borrower1");
+            assert_eq!(fnd_br, Some(Borrower::new("Borrower1", 1)));
+
+            assert_eq!(brs2.len(), 0);
+            let brs2 = add_item(brs2, br2);
+            assert_eq!(brs2.len(), 1);
+            let fnd_br = find_borrower(brs2,"Borrower11");
+            assert_eq!(fnd_br, None);
+
+            assert_eq!(bks1.len(), 0);
+            let bks1 = add_item(bks1, bk1);
+            assert_eq!(bks1.len(), 1);
+
+            let fnd_bk = find_borrower(brs1, "Borrower1");
+            assert_eq!(fnd_br, Some(Borrower::new("Borrower1", 1)));
+
+            assert_eq!(brs2.len(), 0);
+            let brs2 = add_item(brs2, br2);
+            assert_eq!(brs2.len(), 1);
+            let fnd_br = find_borrower(brs2,"Borrower11");
+            assert_eq!(fnd_br, None);
+        }
     //
     //    #[test]
     //    fn test_num_books_out() {
