57be4f9058d49790e3aec2023fdf2efb20172f46
Eric Tobin <eatobin@gmail.com>
2019-10-07 15:44:07 -0500

    progress

--- src/borrower.rs
+++ src/borrower.rs
@@ -42,28 +42,36 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_borrower() {
+    fn test_borrower_construct() {
         let br1 = Borrower::new("Borrower1", 1);
 
-        // test construct
         assert_eq!(Borrower::get_name(&br1), "Borrower1");
         assert_eq!(Borrower::get_max_books(&br1), 1);
+    }
 
-        // test set_name
+    #[test]
+    fn test_set_name() {
+        let br1 = Borrower::new("Borrower1", 1);
         let br2 = Borrower {
             name: "Borrower2".to_owned(),
             max_books: 1,
         };
         assert_eq!(Borrower::set_name(br1.clone(), "Borrower2"), br2);
+    }
 
-        // test set max_books
+    #[test]
+    fn test_set_max_books() {
+        let br1 = Borrower::new("Borrower1", 1);
         let br2 = Borrower {
             name: "Borrower1".to_owned(),
             max_books: 2,
         };
         assert_eq!(Borrower::set_max_books(br1.clone(), 2), br2);
+    }
 
-        // test to_string
+    #[test]
+    fn test_test_to_string() {
+        let br1 = Borrower::new("Borrower1", 1);
         assert_eq!(br1.borrower_to_string(), "Borrower1 (1 books)");
     }
 }
