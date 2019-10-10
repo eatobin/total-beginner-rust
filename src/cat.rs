1c4c6dacda3cbaa2321f47cfab42f241d1b0cc1d
Eric Tobin <eatobin@gmail.com>
2019-10-10 13:08:43 -0500

    cats

--- src/cat.rs
+++ src/cat.rs
@@ -1,4 +1,4 @@
-#[derive(PartialEq, PartialOrd)]
+#[derive(PartialEq, PartialOrd, Debug)]
 pub struct Cat {
     rank: u8,
 }
@@ -30,5 +30,18 @@ mod tests {
         let cat1 = Cat {rank: 44};
         let cat2 = Cat {rank: 4};
         assert_eq!(cat1 > cat2, true)
+    }
+
+    #[test]
+    fn test_cat_vec() {
+        let cat44 = Cat {rank: 44};
+        let cat4 = Cat {rank: 4};
+        let cat16 = Cat {rank: 16};
+        let cats = vec![cat44,cat4,cat16];
+        let mut iterator = cats.iter();
+        assert_eq!(iterator.next(), Some(&Cat {rank: 44}));
+        assert_eq!(iterator.next(), Some(&Cat {rank: 4}));
+        assert_eq!(iterator.nth(0), Some(&Cat {rank: 16}));
+        assert_eq!(iterator.nth(0), None)
     }
 }
