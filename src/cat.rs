464fae1b07039de9a6a43dd32afb7bbb428804f3
Eric Tobin <eatobin@gmail.com>
2019-10-10 12:05:19 -0500

    cats

--- src/cat.rs
+++ src/cat.rs
@@ -1,3 +1,4 @@
+#[derive(PartialEq, PartialOrd)]
 pub struct Cat {
     rank: u8,
 }
@@ -15,5 +16,19 @@ mod tests {
     fn test_cat() {
         let cat1 = Cat { rank: 62 };
         assert_eq!(cat1.get_rank(), 62)
+    }
+
+    #[test]
+    fn test_cat_eq() {
+        let cat1 = Cat {rank: 4};
+        let cat2 = Cat {rank: 4};
+        assert_eq!(cat1 == cat2, true)
+    }
+
+    #[test]
+    fn test_cat_gt() {
+        let cat1 = Cat {rank: 44};
+        let cat2 = Cat {rank: 4};
+        assert_eq!(cat1 > cat2, true)
     }
 }
