61ee008927be196ddd964b71a4eabf5d5d8d7574
Eric Tobin <eatobin@gmail.com>
2019-10-10 13:27:41 -0500

    cats

--- src/cat.rs
+++ src/cat.rs
@@ -20,28 +20,41 @@ mod tests {
 
     #[test]
     fn test_cat_eq() {
-        let cat1 = Cat {rank: 4};
-        let cat2 = Cat {rank: 4};
+        let cat1 = Cat { rank: 4 };
+        let cat2 = Cat { rank: 4 };
         assert_eq!(cat1 == cat2, true)
     }
 
     #[test]
     fn test_cat_gt() {
-        let cat1 = Cat {rank: 44};
-        let cat2 = Cat {rank: 4};
+        let cat1 = Cat { rank: 44 };
+        let cat2 = Cat { rank: 4 };
         assert_eq!(cat1 > cat2, true)
     }
 
     #[test]
-    fn test_cat_vec() {
-        let cat44 = Cat {rank: 44};
-        let cat4 = Cat {rank: 4};
-        let cat16 = Cat {rank: 16};
-        let cats = vec![cat44,cat4,cat16];
+    fn test_cat_vec_owned() {
+        let cat44 = Cat { rank: 44 };
+        let cat4 = Cat { rank: 4 };
+        let cat16 = Cat { rank: 16 };
+        let cats = vec![cat44, cat4, cat16];
         let mut iterator = cats.iter();
-        assert_eq!(iterator.next(), Some(&Cat {rank: 44}));
-        assert_eq!(iterator.next(), Some(&Cat {rank: 4}));
-        assert_eq!(iterator.nth(0), Some(&Cat {rank: 16}));
+        assert_eq!(iterator.next(), Some(&Cat { rank: 44 }));
+        assert_eq!(iterator.next(), Some(&Cat { rank: 4 }));
+        assert_eq!(iterator.nth(0), Some(&Cat { rank: 16 }));
+        assert_eq!(iterator.nth(0), None)
+    }
+
+    #[test]
+    fn test_cat_vec_refs() {
+        let cat44 = Cat { rank: 44 };
+        let cat4 = Cat { rank: 4 };
+        let cat16 = Cat { rank: 16 };
+        let cats = vec![&cat44, &cat4, &cat16];
+        let mut iterator = cats.iter();
+        assert_eq!(iterator.next(), Some(&&Cat {rank: 44}));
+        assert_eq!(iterator.next(), Some(&&Cat {rank: 4}));
+        assert_eq!(iterator.nth(0), Some(&&Cat {rank: 16}));
         assert_eq!(iterator.nth(0), None)
     }
 }
