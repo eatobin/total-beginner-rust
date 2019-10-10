e9b78bdeea6fa499b192c7814ecd9cc3dcf81f25
Eric Tobin <eatobin@gmail.com>
2019-10-10 13:47:53 -0500

    i understand!

--- src/cat.rs
+++ src/cat.rs
@@ -54,10 +54,10 @@ mod tests {
         let cat16 = Cat { rank: 16 };
         let cats = vec![&cat44, &cat4, &cat16];
         let mut iterator = cats.iter();
-        assert_eq!(iterator.next(), Some(&&Cat {rank: 44}));
+        assert_eq!(iterator.next(), Some(&&Cat { rank: 44 }));
         assert_eq!(iterator.len(), 2);
-        assert_eq!(iterator.next(), Some(&&Cat {rank: 4}));
-        assert_eq!(iterator.nth(0), Some(&&Cat {rank: 16}));
+        assert_eq!(iterator.next(), Some(&&Cat { rank: 4 }));
+        assert_eq!(iterator.nth(0), Some(&&Cat { rank: 16 }));
         assert_eq!(iterator.nth(0), None);
         assert_eq!(iterator.len(), 0)
     }
@@ -73,6 +73,21 @@ mod tests {
         assert_eq!(iterator.len(), 2);
         assert_eq!(iterator.next(), Some(Cat { rank: 4 }));
         assert_eq!(iterator.nth(0), Some(Cat { rank: 16 }));
+        assert_eq!(iterator.nth(0), None);
+        assert_eq!(iterator.len(), 0)
+    }
+
+    #[test]
+    fn test_cat_vec_refs_into() {
+        let cat44 = Cat { rank: 44 };
+        let cat4 = Cat { rank: 4 };
+        let cat16 = Cat { rank: 16 };
+        let cats = vec![&cat44, &cat4, &cat16];
+        let mut iterator = cats.into_iter();
+        assert_eq!(iterator.next(), Some(&Cat { rank: 44 }));
+        assert_eq!(iterator.len(), 2);
+        assert_eq!(iterator.next(), Some(&Cat { rank: 4 }));
+        assert_eq!(iterator.nth(0), Some(&Cat { rank: 16 }));
         assert_eq!(iterator.nth(0), None);
         assert_eq!(iterator.len(), 0)
     }
