99f2d1be9b82931a20210e08e9ca9de518b80f02
Eric Tobin <eatobin@gmail.com>
2019-10-12 21:51:09 -0500

    progress

--- src/book.rs
+++ src/book.rs
@@ -1,116 +1,116 @@
-use crate::borrower::Borrower;
+//use crate::borrower::Borrower;
 
-#[derive(Debug, PartialEq, Clone)]
-pub struct Book<'a> {
-    title: String,
-    author: String,
-    borrower: Option<&'a Borrower>,
-}
+//#[derive(Debug, PartialEq, Clone)]
+//pub struct Book<'a> {
+//    title: String,
+//    author: String,
+//    borrower: Option<&'a Borrower>,
+//}
+//
+//impl<'a> Book<'a> {
+//    pub fn new(title: &str, author: &str, borrower: Option<&'a Borrower>) -> Book<'a> {
+//        Book {
+//            title: title.to_owned(),
+//            author: author.to_owned(),
+//            borrower,
+//        }
+//    }
+//
+//    pub fn get_title(&self) -> &str {
+//        &(self.title)
+//    }
+//
+//    pub fn set_title(self, title: &str) -> Self {
+//        Self {
+//            title: title.to_owned(),
+//            ..self
+//        }
+//    }
+//
+//    pub fn get_author(&self) -> &str {
+//        &(self.author)
+//    }
+//
+//    pub fn set_author(self, author: &str) -> Self {
+//        Self {
+//            author: author.to_owned(),
+//            ..self
+//        }
+//    }
+//
+//    pub fn get_borrower(&self) -> Option<&Borrower> {
+//        self.borrower
+//    }
+//
+//    pub fn set_borrower(self, borrower: Option<&'a Borrower>) -> Self {
+//        Self { borrower, ..self }
+//    }
 
-impl<'a> Book<'a> {
-    pub fn new(title: &str, author: &str, borrower: Option<&'a Borrower>) -> Book<'a> {
-        Book {
-            title: title.to_owned(),
-            author: author.to_owned(),
-            borrower,
-        }
-    }
+//    pub fn available_string(&self) -> String {
+//        match &self.borrower {
+//            Some(br) => format!("{} {}", "Checked out to", br.get_name()),
+//            None => "Available".to_owned(),
+//        }
+//    }
+//
+//    pub fn book_to_string(&self) -> String {
+//        format!(
+//            "{} {} {}{} {}",
+//            &(self.title),
+//            "by",
+//            &(self.author),
+//            ";",
+//            &(self.available_string())
+//        )
+//    }
+//}
 
-    pub fn get_title(&self) -> &str {
-        &(self.title)
-    }
-
-    pub fn set_title(self, title: &str) -> Self {
-        Self {
-            title: title.to_owned(),
-            ..self
-        }
-    }
-
-    pub fn get_author(&self) -> &str {
-        &(self.author)
-    }
-
-    pub fn set_author(self, author: &str) -> Self {
-        Self {
-            author: author.to_owned(),
-            ..self
-        }
-    }
-
-    pub fn get_borrower(&self) -> Option<&Borrower> {
-        self.borrower
-    }
-
-    pub fn set_borrower(self, borrower: Option<&'a Borrower>) -> Self {
-        Self { borrower, ..self }
-    }
-
-    pub fn available_string(&self) -> String {
-        match &self.borrower {
-            Some(br) => format!("{} {}", "Checked out to", br.get_name()),
-            None => "Available".to_owned(),
-        }
-    }
-
-    pub fn book_to_string(&self) -> String {
-        format!(
-            "{} {} {}{} {}",
-            &(self.title),
-            "by",
-            &(self.author),
-            ";",
-            &(self.available_string())
-        )
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::*;
-
-    #[test]
-    fn test_book_construct() {
-        let bk1 = Book::new("Title1", "Author1", None);
-        assert_eq!(bk1.get_title(), "Title1");
-        assert_eq!(bk1.get_author(), "Author1");
-        let br1 = Borrower::new("Borrower1", 1);
-        let sbr1 = Some(&br1);
-        let bk2 = Book::new("Title1", "Author1", sbr1);
-        assert_eq!(bk2.get_borrower(), (Some(&Borrower::new("Borrower1", 1))));
-        assert_eq!(bk1.get_borrower(), None);
-    }
-
-    #[test]
-    fn test_book_set_values() {
-        let br1 = Borrower::new("Borrower1", 1);
-        let sbr1 = Some(&br1);
-        let bk1 = Book::new("Title1", "Author1", None);
-        let bk2 = Book::new("Title1", "Author1", sbr1);
-        let bk3 = Book::new("Title2", "Author1", None);
-        assert_eq!(bk1.clone().set_title("Title2"), bk3);
-        let bk4 = Book::new("Title1", "Author2", None);
-        assert_eq!(bk1.clone().set_author("Author2"), bk4);
-        assert_eq!(
-            bk1.clone()
-                .set_borrower(Some(&Borrower::new("Borrower1", 1))),
-            bk2
-        );
-    }
-
-    #[test]
-    fn test_book_to_string() {
-        let br1 = Borrower::new("Borrower1", 1);
-        let sbr1 = Some(&br1);
-        let bk1 = Book::new("Title1", "Author1", None);
-        let bk2 = Book::new("Title1", "Author1", sbr1);
-        assert_eq!(bk1.available_string(), "Available");
-        assert_eq!(bk2.available_string(), "Checked out to Borrower1");
-        assert_eq!(bk1.book_to_string(), "Title1 by Author1; Available");
-        assert_eq!(
-            bk2.book_to_string(),
-            "Title1 by Author1; Checked out to Borrower1"
-        );
-    }
-}
+//#[cfg(test)]
+//mod tests {
+//    // Note this useful idiom: importing names from outer (for mod tests) scope.
+//    use super::*;
+//
+//    #[test]
+//    fn test_book_construct() {
+//        let bk1 = Book::new("Title1", "Author1", None);
+//        assert_eq!(bk1.get_title(), "Title1");
+//        assert_eq!(bk1.get_author(), "Author1");
+//        let br1 = Borrower::new("Borrower1", 1);
+//        let sbr1 = Some(&br1);
+//        let bk2 = Book::new("Title1", "Author1", sbr1);
+//        assert_eq!(bk2.get_borrower(), (Some(&Borrower::new("Borrower1", 1))));
+//        assert_eq!(bk1.get_borrower(), None);
+//    }
+//
+//    #[test]
+//    fn test_book_set_values() {
+//        let br1 = Borrower::new("Borrower1", 1);
+//        let sbr1 = Some(&br1);
+//        let bk1 = Book::new("Title1", "Author1", None);
+//        let bk2 = Book::new("Title1", "Author1", sbr1);
+//        let bk3 = Book::new("Title2", "Author1", None);
+//        assert_eq!(bk1.clone().set_title("Title2"), bk3);
+//        let bk4 = Book::new("Title1", "Author2", None);
+//        assert_eq!(bk1.clone().set_author("Author2"), bk4);
+//        assert_eq!(
+//            bk1.clone()
+//                .set_borrower(Some(&Borrower::new("Borrower1", 1))),
+//            bk2
+//        );
+//    }
+//
+//    #[test]
+//    fn test_book_to_string() {
+//        let br1 = Borrower::new("Borrower1", 1);
+//        let sbr1 = Some(&br1);
+//        let bk1 = Book::new("Title1", "Author1", None);
+//        let bk2 = Book::new("Title1", "Author1", sbr1);
+//        assert_eq!(bk1.available_string(), "Available");
+//        assert_eq!(bk2.available_string(), "Checked out to Borrower1");
+//        assert_eq!(bk1.book_to_string(), "Title1 by Author1; Available");
+//        assert_eq!(
+//            bk2.book_to_string(),
+//            "Title1 by Author1; Checked out to Borrower1"
+//        );
+//    }
+//}
--- src/borrower.rs
+++ src/borrower.rs
@@ -12,24 +12,9 @@ impl Borrower {
         }
     }
 
-    pub fn get_name(&self) -> &str {
-        &(self.name)
-    }
+    pub fn set_name(&mut self, name: &str) { self.name = name.to_owned() }
 
-    pub fn get_max_books(&self) -> u8 {
-        self.max_books
-    }
-
-    pub fn set_name(self, name: &str) -> Self {
-        Self {
-            name: name.to_owned(),
-            ..self
-        }
-    }
-
-    pub fn set_max_books(self, max_books: u8) -> Self {
-        Self { max_books, ..self }
-    }
+    pub fn set_max_books(&mut self, max_books: u8) { self.max_books = max_books }
 
     pub fn borrower_to_string(&self) -> String {
         format!("{} {}{} {}", &(self.name), "(", &(self.max_books), "books)")
@@ -44,33 +29,34 @@ mod tests {
     #[test]
     fn test_borrower_construct() {
         let br1 = Borrower::new("Borrower1", 1);
-        assert_eq!(Borrower::get_name(&br1), "Borrower1");
-        assert_eq!(Borrower::get_max_books(&br1), 1);
+        assert_eq!(br1.name, "Borrower1");
+        assert_eq!(br1.max_books, 1);
     }
 
     #[test]
     fn test_set_name() {
-        let br1 = Borrower::new("Borrower1", 1);
-        let br2 = Borrower {
+        let br1 = &mut Borrower::new("Borrower1", 1);
+        let br2 = &mut Borrower {
             name: "Borrower2".to_owned(),
             max_books: 1,
         };
-        assert_eq!(Borrower::set_name(br1, "Borrower2"), br2);
+        br1.set_name("Borrower2");
+        assert_eq!(br1, br2);
     }
 
-    #[test]
-    fn test_set_max_books() {
-        let br1 = Borrower::new("Borrower1", 1);
-        let br2 = Borrower {
-            name: "Borrower1".to_owned(),
-            max_books: 2,
-        };
-        assert_eq!(Borrower::set_max_books(br1, 2), br2);
-    }
-
-    #[test]
-    fn test_test_to_string() {
-        let br1 = Borrower::new("Borrower1", 1);
-        assert_eq!(br1.borrower_to_string(), "Borrower1 (1 books)");
-    }
+//    #[test]
+//    fn test_set_max_books() {
+//        let br1 = Borrower::new("Borrower1", 1);
+//        let br2 = Borrower {
+//            name: "Borrower1".to_owned(),
+//            max_books: 2,
+//        };
+//        assert_eq!(Borrower::set_max_books(br1, 2), br2);
+//    }
+//
+//    #[test]
+//    fn test_test_to_string() {
+//        let br1 = Borrower::new("Borrower1", 1);
+//        assert_eq!(br1.borrower_to_string(), "Borrower1 (1 books)");
+//    }
 }
